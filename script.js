29b6918cd030b91277a419799ec23e603c76fa4f
mirzad20 <mirzadsami2116@gmail.com>
2024-02-15 12:34:09 +0430

    modified the unlike button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let likebtn= [document.getElementById('like0'),document.getElementById('like1'),document.getElementById('like2')];
 let likes = [document.getElementById('likes0'),document.getElementById('likes1'),document.getElementById('likes2')]
-let post0 = document.getElementById('post0');
+// let post0 = document.getElementById('post0');    
  
 
 const images = document.querySelectorAll('img');
@@ -20,16 +20,21 @@ const images = document.querySelectorAll('img');
 
 
         let srcAtt = likebtn[i].getAttribute('src');
+            let originalText = likes[i].textContent;
     if(srcAtt.includes('icon')){
 
     likebtn[i].src = 'filledheart.png';
     likebtn[i].classList = 'rounded';
-    likes[i].textContent = 'Likedy by you, shamstabrizi44 and 231323 others';
+    // likes[i].textContent = 'Likedy by you, shamstabrizi44 and 231323 others';
+    let positions = likes[i].textContent;
+    let position = positions.indexOf('y')+2;
+    
+     likes[i].textContent = `${likes[i].textContent.slice(0,position) } you , ${likes[i].textContent.slice(position)}`
     
     }
     else{
     likebtn[i].src = 'icon-heart.png'
-    likes[i].textContent = 'Liked by shamstabrizi44 and 231323 others';
+    likes[i].textContent =likes[i].textContent.split('you ,').join("").trim();
 
     }
         }
