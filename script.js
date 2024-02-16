97b31bbac75226f7874234c8718b8a7b190afb0b
mirzad20 <mirzadsami2116@gmail.com>
2024-02-16 10:48:02 +0430

    final version of the app

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let likebtn= [document.getElementById('like0'),document.getElementById('like1'),document.getElementById('like2')];
-let likes = [document.getElementById('likes0'),document.getElementById('likes1'),document.getElementById('likes2')]
+let likebtn= [document.getElementById('like0'),document.getElementById('like1'),document.getElementById('like2'),document.getElementById('like3')];
+let likes = [document.getElementById('likes0'),document.getElementById('likes1'),document.getElementById('likes2'),document.getElementById('likes3')]
 // let post0 = document.getElementById('post0');    
  
 
@@ -25,8 +25,7 @@ const images = document.querySelectorAll('img');
 
     likebtn[i].src = 'filledheart.png';
     likebtn[i].classList = 'rounded';
-    // likes[i].textContent = 'Likedy by you, shamstabrizi44 and 231323 others';
-    let positions = likes[i].textContent;
+     let positions = likes[i].textContent;
     let position = positions.indexOf('y')+2;
     
      likes[i].textContent = `${likes[i].textContent.slice(0,position) } you , ${likes[i].textContent.slice(position)}`
@@ -41,3 +40,33 @@ const images = document.querySelectorAll('img');
         // Do whatever you want with the image ID here
       });
     });
+
+    images.forEach(image => {
+        image.addEventListener('dblclick', function() {
+          // Get the id of the clicked image
+          const attr = image.getAttribute('id');
+  
+          if( attr.includes('post')){
+          const imageId = this.id;
+          
+          const i = imageId.charAt(imageId.length-1);
+          console.log('Clicked image ID:', imageId , i);
+  
+  
+  
+          let srcAtt = likebtn[i].getAttribute('src');
+              let originalText = likes[i].textContent;
+      if(srcAtt.includes('icon')){
+  
+      likebtn[i].src = 'filledheart.png';
+      likebtn[i].classList = 'rounded';
+       let positions = likes[i].textContent;
+      let position = positions.indexOf('y')+2;
+      
+       likes[i].textContent = `${likes[i].textContent.slice(0,position) } you , ${likes[i].textContent.slice(position)}`
+      
+      }
+    }
+    // Do whatever you want with the image ID here
+  });
+});
